d0fd9dbe864823120fcef201c171f97c19dd2608
vhoang <vhoang@db1.(none)>
2015-03-02 08:13:27 -0500

    Corrected output bug

--- scripts/spatialjoin2.sh
+++ scripts/spatialjoin2.sh
@@ -9,10 +9,11 @@ usage(){
   -q DISTANCE, --qdistance=DISTANCE \t Distance (for dwithin predicate) \n \
   -d DESTINATION_PATH, --destination=DESTINATION_PATH\tThe HDFS prefix path to store the query result \n \
   -f FIELDS_TO_OUTPUT, --fields=FIELDS_TO_OUTPUT \t The fields to be included in the outputs. The format is comma-separated for fields within 1 data set, and separated by color between datasets. E.g. --fields=1,3,4:2,5,9 \n \
-  -n NUMBER_REDUCERS, --num_reducers=NUM_REDUCERS \t Number of reducers to be used \n \
-  -s TRUE_OR_FALSE, --statistics=TRUE_OR_FALSE \t Appending additional spatial join statistics to joined pairs: [true | false]. \n \
+  -n NUMBER_REDUCERS, --numreducers=NUM_REDUCERS \t Number of reducers to be used \n \
+  -s TRUE_OR_FALSE, --statistics=TRUE_OR_FALSE \t Appending additional spatial join statistics to joined pairs: [true | false]. The default is false. \n \
+  -t TRUE_OR_FALSE, --tileid=TRUE_OR_FALSE \t Appending (keeping) the tile id as the last field appended to the output [true | false]. The default is false. \n \
   -m PARTITION_METHOD, --method=PARTITION_METHOD \t OPTIONAL - The partitioning method. The default method is fixed grid partitioning. [ fg | bsp ] \n \
-   -r SAMPLING_RATIO, --ratio=SAMPLING_RATIO \t OPTIONAL - The sampling ratio for partitioning the data. Default value is 1.0."
+  -r SAMPLING_RATIO, --ratio=SAMPLING_RATIO \t OPTIONAL - The sampling ratio for partitioning the data. Default value is 1.0."
  # -i OBJECT_ID, --obj_id=OBJECT_ID \t The field (position) of the object ID \n \
   exit 1
 }
@@ -29,12 +30,14 @@ prefixpath2=""
 geomid=""
 delimiter=""
 predicate=""
-sample_ratio=1
+sampleratio=1
 method="fg"
 statistics="false"
-num_reducers=""
+tileid="false"
+numreducers=""
 qdistance=0
 fields=""
+deduparg="uniq"
 
 while : 
 do
@@ -83,12 +86,12 @@ do
           qdistance=${1#*=}
           shift
           ;;
-        -n | --num_reducers)
-          num_reducers=$2
+        -n | --numreducers)
+          numreducers=$2
           shift 2
           ;;
-        --num_reducers=*)
-          num_reducers=${1#*=}
+        --numreducers=*)
+          numreducers=${1#*=}
           shift
           ;;
         -s | --statistics)
@@ -99,12 +102,20 @@ do
           statistics=${1#*=}
           shift
           ;;
+        -t | --tileid)
+          tileid=$2
+          shift 2
+          ;;
+        --tileid=*)
+          tileid=${1#*=}
+          shift
+          ;;
         -r | --ratio)
-          sample_ratio=$2
+          sampleratio=$2
           shift 2
           ;;
         --ratio=*)
-          sample_ratio=${1#*=}
+          sampleratio=${1#*=}
           shift
           ;;
         -m | --method)
@@ -143,6 +154,7 @@ if [ -e "${SATO_CONFIG}" ]; then
   source ${SATO_CONFIG}
 else
   echo "SATO configuration file not found!"
+  exit 1
 fi
 
 LD_CONFIG_PATH=${LD_LIBRARY_PATH}:${SATO_LIB_PATH}
@@ -157,12 +169,13 @@ if [ ! "${destination}" ]; then
   exit 1
 fi
 
+
 if [ "${predicate}" != "intersects" ] && [ "${predicate}" != "touches" ] && [ "${predicate}" != "crosses" ] && [ "${predicate}" != "contains" ] && [ "${predicate}" != "adjacent" ] && [ "${predicate}" != "disjoint" ] && [ "${predicate}" != "equals" ] && [ "${predicate}" != "dwithin" ] && [ "${predicate}" != "within" ] && [ "${predicate}" != "overlaps" ]; then
    echo "ERROR: Invalid predicate. See --help" >&2
    exit 1
 fi
 
-if ! [[ ${num_reducers} -ge 1 ]]; then
+if ! [[ ${numreducers} -ge 1 ]]; then
   echo "ERROR: Missing the number of reducers. See --help" >&2
   exit 1
 fi
@@ -170,6 +183,16 @@ fi
 if ! [ "${method}" == "fg" ] && ! [ "${method}" == "bsp" ] ; then
    echo "Invalid partitioning method"
    exit 1
+fi
+
+fieldsarg=""
+if [ "${fields}" ] ; then
+   fieldsarg="-f ${fields}"
+fi
+tileidarg=""
+if [ "${tileid}" ] ; then
+   tileidarg="-t ${tileid}"
+   deduparg="uniq2"
 fi
 
 # Creating the path with the HDFS prefix
@@ -239,8 +262,7 @@ MAPPER_1_PATH=../step_analyze/mbbnorm.py
 hdfs dfs -rm -f -r ${OUTPUT_1}
 
 echo "Normalizing MBBs"
-# Normalizing only MBB of set 2
-hadoop jar ${HJAR} -input ${INPUT_1B} -output ${OUTPUT_1} -file ${MAPPER_1_PATH} -mapper "${MAPPER_1} ${min_x} ${min_y} ${max_x} ${max_y}" -reducer None -numReduceTasks 0
+hadoop jar ${HJAR} -input ${INPUT_1A} -input ${INPUT_1B} -output ${OUTPUT_1} -file ${MAPPER_1_PATH} -mapper "${MAPPER_1} ${min_x} ${min_y} ${max_x} ${max_y}" -reducer None -numReduceTasks 0
 
 if [  $? -ne 0 ]; then
    echo "Normalizing MBB has failed!"
@@ -248,7 +270,7 @@ if [  $? -ne 0 ]; then
 fi
 
 # Determine the optimal bucket count
-partitionSize=20
+partitionSize=5000
 
 echo "partitionsize=${partitionSize}"
 
@@ -267,17 +289,16 @@ if [ "$method" == "fg" ]; then
    ../step_tear/fg/serial/fgNoMbb.py ${min_x} ${min_y} ${max_x} ${max_y} ${partitionSize} ${numobjects} > ${PARTITION_FILE}
 fi
 
+samplepartsize=`(../step_analyze/computeSamplePartSize.py ${partitionSize} ${sampleratio} 1)`
 if [ "$method" == "bsp" ]; then
-   ../step_tear/bsp/serial/bsp -b ${partitionSize} -i ${INPUT_MBB_FILE} > ${PARTITION_FILE}
+   ../step_tear/bsp/serial/bsp -b ${samplepartsize} -i ${INPUT_MBB_FILE} > ${PARTITION_FILE}
 fi
 cat ${PARTITION_FILE}
 echo "Done partitioning"
-
 # Remove temporary files
 rm ${INPUT_MBB_FILE}
 
 PARTITION_FILE_DENORM=partfiledenorm
-
 # Denormalize the MBB file and copy them to HDFS
 python ../step_tear/denormalize.py ${min_x} ${min_y} ${max_x} ${max_y}  < ${PARTITION_FILE} > ${PARTITION_FILE_DENORM}
 
@@ -297,15 +318,14 @@ REDUCER_2_PATH=../joiner/resque
 hdfs dfs -rm -f -r ${OUTPUT_2}
 
 
-hdfs dfs -rm -f -r ${OUTPUT_1}
 predicate="st_"${predicate}
 
 echo "${MAPPER_2} ${geomid1} ${geomid2} ${SATO_INDEX_FILE_NAME} ${prefixpath1} ${prefixpath2}"
-echo "${REDUCER_2} -p ${predicate} -i ${geomid1} -j ${geomid2} -s ${statistics}"
-
+echo "${REDUCER_2} -p ${predicate} -i ${geomid1} -j ${geomid2} -s ${statistics} -d ${qdistance} ${fieldsarg} ${tileidarg}"
 
 #Perform spatial join
-hadoop jar ${HJAR} -input ${INPUT_2A} -input ${INPUT_2B} -output ${OUTPUT_2} -file ${MAPPER_2_PATH} -file ${REDUCER_2_PATH} -file ${SATO_INDEX_FILE_NAME}  -mapper "${MAPPER_2} ${geomid1} ${geomid2} ${SATO_INDEX_FILE_NAME} ${prefixpath1} ${prefixpath2}" -reducer "${REDUCER_2} -p ${predicate} -i ${geomid1} -j ${geomid2} -s ${statistics} -d ${qdistance} -f ${fields} " -cmdenv LD_LIBRARY_PATH=${LD_CONFIG_PATH} -numReduceTasks ${num_reducers}
+#hadoop jar ${HJAR} -input ${INPUT_2A} -input ${INPUT_2B} -output ${OUTPUT_2} -file ${MAPPER_2_PATH} -file ${REDUCER_2_PATH} -file ${SATO_INDEX_FILE_NAME}  -mapper "${MAPPER_2} ${geomid1} ${geomid2} ${SATO_INDEX_FILE_NAME} ${prefixpath1} ${prefixpath2}" -reducer "${REDUCER_2} -p ${predicate} -i ${geomid1} -j ${geomid2} -s ${statistics} -d ${qdistance} ${fieldsarg} ${tileidarg}" -cmdenv LD_LIBRARY_PATH=${LD_CONFIG_PATH} -numReduceTasks ${numreducers}
+hadoop jar ${HJAR} -input ${INPUT_2A} -input ${INPUT_2B} -output ${OUTPUT_2} -file ${MAPPER_2_PATH} -file ${REDUCER_2_PATH} -file ${SATO_INDEX_FILE_NAME}  -mapper "${MAPPER_2} ${geomid1} ${geomid2} ${SATO_INDEX_FILE_NAME} ${prefixpath1} ${prefixpath2}" -reducer "${REDUCER_2} -p ${predicate} -i ${geomid1} -j ${geomid2} -s ${statistics} -d ${qdistance} ${fieldsarg} ${tileidarg}" -cmdenv LD_LIBRARY_PATH=${LD_CONFIG_PATH} -numReduceTasks 0
 
 if [  $? -ne 0 ]; then
    echo "Spatial computation has failed!"
@@ -322,14 +342,14 @@ REDUCER_3_PATH=../joiner/hgdeduplicater.py
 hdfs dfs -rm -r ${OUTPUT_3}
 echo -e "Deduplication step" 
 
-hadoop jar ${HJAR} -file ${REDUCER_3_PATH} -mapper 'cat -' -reducer "${REDUCER_3} uniq" -input ${INPUT_3} -output ${OUTPUT_3} -numReduceTasks ${num_reducers} -jobconf mapred.task.timeout=360000000
+hadoop jar ${HJAR} -file ${REDUCER_3_PATH} -mapper 'cat -' -reducer "${REDUCER_3} ${deduparg}" -input ${INPUT_3} -output ${OUTPUT_3} -numReduceTasks ${numreducers} -jobconf mapred.task.timeout=360000000
 
 succ=$?
 
-hdfs dfs -rm -r ${OUTPUT_2}
 if [[ $succ != 0 ]] ; then
     echo -e "\n\n deduplication stage has failed. \nPlease check the output result for debugging."
     exit $succ
 fi
+hdfs dfs -rm -r -f ${OUTPUT_2}
 
 echo "Done. Results are available at ${OUTPUT_3}"
